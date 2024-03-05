285c5a92266d94be40786663450226940285727e
paul <>
2024-03-05 15:12:58 +0800

    install next@latest and build infrastructure

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function StopWatchApp() {
+  return (
+    <main>
+      {/* Put your components here */}
+      <h2>00:00.00</h2>
+      <button>Lap</button>
+      <button>Start</button>
+    </main>
+  )
+}
